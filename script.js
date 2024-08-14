116cac131a41c602d3e62538aa3bbd5fb12e3c15
= <fuxx222.anmeldung@gmail.com>
2024-08-14 18:42:48 +0200

    Upload session 2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const youtubeUrl = "https://www.youtube.com";
+const youtubeSearchResultsUrl = `${youtubeUrl}/results`;
 let studyMode = true;
 
-if (window.location.href.includes(youtubeUrl)) {
+if (window.location.href.includes(youtubeUrl) && !window.location.href.includes('/watch') && !window.location.href.includes('/results')) {
     if (studyMode) {
         const body = document.querySelector('body');
 
@@ -20,5 +21,44 @@ if (window.location.href.includes(youtubeUrl)) {
 
     } else {
         console.log("Study Mode is off");
+    }
+}
+
+function removeElementsFromSearch() {
+    const personalBar = document.querySelector('#guide');
+    const mastheadContainer = document.querySelector('#masthead-container');
+    const dismissables = document.querySelectorAll('ytd-shelf-renderer');
+    const reelShelves = document.querySelectorAll('ytd-reel-shelf-renderer');
+
+    if(personalBar) { personalBar.remove();}
+    if(mastheadContainer) { mastheadContainer.remove();}
+
+    dismissables.forEach(function(dismissable) {
+        dismissable.remove();
+    });
+
+
+    reelShelves.forEach(function(reelShelf) {
+        reelShelf.remove();
+    });
+}
+
+
+
+if(window.location.href.includes(youtubeSearchResultsUrl)) {
+    if (studyMode) {
+        removeElementsFromSearch();
+    } else {
+        console.log("Study Mode is off");
+    }
+}
+
+window.onscroll = function() {
+    if (window.location.href.includes(youtubeSearchResultsUrl)) {
+        if (studyMode) {
+            removeElementsFromSearch();
+        } else {
+            console.log("Study Mode is off");
+        }
     }
 }
